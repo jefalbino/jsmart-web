3412b03e89e05f0586293d8aa7f1fefc6909b33b
jefalbino <jef.albino@yahoo.com.br>
2015-08-11 20:37:37 -0300

    Changed jsmart5 to be LGPL 3.0

--- src/main/java/com/jsmart5/framework/manager/RequestPathFilter.java
+++ src/main/java/com/jsmart5/framework/manager/RequestPathFilter.java
@@ -3,7 +3,7 @@
  * Copyright (c) 2014, Jeferson Albino da Silva, All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public
+ * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
  *
@@ -12,7 +12,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public
+ * You should have received a copy of the GNU Lesser General Public
  * License along with this library. If not, see <http://www.gnu.org/licenses/>.
 */
 
